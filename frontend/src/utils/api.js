402c36602439af06170ce62a7bb810307b70ce10
Murali Krishna <muralikrishna8309@gmail.com>
2026-04-24 07:35:21 +0530

    added .env file and accessed vaibles through env

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import { store } from '../store/store';
 import { logout } from '../store/slices/authSlice';
 import toast from 'react-hot-toast';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
